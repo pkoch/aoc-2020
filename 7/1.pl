65d677aab6625c067e124d0af1b79e94bc10fe41
Paulo Köch <paulo.koch@gmail.com>
2020-12-16 21:17:31 +0000

    Solve 7.1

--- 7/1.pl
+++ 7/1.pl
@@ -1,10 +1,11 @@
 %
+:- use_module(library(pio)).
 :- use_module(library(dcg/basics)).
 :- use_module(library(dcg/high_order)).
 
 satom(Atom) --> string(Codes), {atom_codes(Atom, Codes)}.
 
-bag_spec((Adjective, Color)) -->
+bag_spec(Adjective-Color) -->
   satom(Adjective),
   white,
   satom(Color),
@@ -12,7 +13,7 @@ bag_spec((Adjective, Color)) -->
   "bag",
   ("s" | []).
 
-counted_bag_spec((Count, BagSpec)) -->
+counted_bag_spec(Count:BagSpec) -->
   number(Count),
   white,
   bag_spec(BagSpec).
@@ -26,4 +27,23 @@ bag_phrase((Container, Containees)) -->
   ),
   ".".
 
-bag_phrases(Ls) --> sequence(bag_phrase, "\n", Ls).
+bag_phrases([]) --> [].
+bag_phrases([H|T]) --> bag_phrase(H), !,"\n", bag_phrases(T).
+
+bag_houses(BSer, BSee, DB):-
+  member((BSer, BSees), DB),
+  member(_:BSee, BSees).
+
+bag_contains(BSer, BSee, DB):-
+  bag_houses(BSer, BSee, DB)
+  ;(
+    bag_houses(BSer, Middle, DB),
+    bag_contains(Middle, BSee, DB)
+  ).
+
+main:-
+  phrase_from_file(bag_phrases(DB), input),
+  setof(Er, bag_contains(Er, shiny-gold, DB), Ls),
+  length(Ls, N),
+  writeln(N),
+halt.
